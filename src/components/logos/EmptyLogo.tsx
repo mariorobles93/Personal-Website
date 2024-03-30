6c5645fa9e6647d0592bcfe8c8313aaa0b3d50dc
Mario Robles <mario.robles.93@gmail.com>
2024-03-30 18:35:06 -0500

    Added empty logo Added profile picture

---
+++ src/components/logos/EmptyLogo.tsx
@@ -0,0 +1,7 @@
+export const EmptyLogo = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg viewBox="0 0 258 61" xmlns="http://www.w3.org/2000/svg" {...props}>
+    </svg>
+
+  );
+};
