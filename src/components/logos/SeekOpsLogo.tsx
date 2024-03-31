eb6e3552c4dffefb69c589ed9aa3244b9c7408dd
Mario Robles <mario.robles.93@gmail.com>
2024-03-30 19:58:52 -0500

    Changed background on seekops logo Changed favicon

--- src/components/logos/SeekOpsLogo.tsx
+++ src/components/logos/SeekOpsLogo.tsx
@@ -1,7 +1,8 @@
 export const SeekOpsLogo = (props: React.SVGProps<SVGSVGElement>) => {
   return (
     <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" {...props}>
-      <image href="/SeekOps.jpeg" width="200"/>
+      <rect x="0" y="0" width="200" height="200" fill="white" />
+      <image href="/SeekOps2.png" width="200" />
     </svg>
   );
 };
